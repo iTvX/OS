852bff3ea6c1292083b93f14df1e47e49f4ba257
Zijie Yu <cszijie@gmail.com>
2020-03-20 17:45:11 -0400

    1

--- HW2/src/test.java
+++ HW2/src/test.java
@@ -1,5 +1,5 @@
 public class test {
-    private static final int[][] sudoku = {
+    private static int[][] source = {
             {6, 2, 4, 5, 3, 9, 1, 8, 7},
             {5, 1, 9, 7, 2, 8, 6, 3, 4},
             {8, 3, 7, 6, 1, 4, 2, 9, 5},
@@ -10,14 +10,13 @@ public class test {
             {4, 9, 6, 1, 8, 2, 5, 7, 3},
             {2, 8, 5, 4, 7, 3, 9, 1, 6}
     };
-    // Array that worker threads will update
-    private static boolean[] valid;
 
-    // Runnable object that determines if numbers 1-9 only appear once in a 3x3 subsection
-    public static class Is3x3Valid implements Runnable {
+    private static boolean[] isValid;
+
+    public static class validator implements Runnable {
         int row;
         int col;
-        Is3x3Valid(int row, int column) {
+        validator(int row, int column) {
             this.row = row;
             this.col = column;
         }
@@ -28,13 +27,13 @@ public class test {
             int product = 1;
             for (int i = row; i < row + 3; i++) {
                 for (int j = col; j < col + 3; j++) {
-                    int num = sudoku[i][j];
+                    int num = source[i][j];
                     sum = num + sum;
                     product *= num;
                 }
             }
             if (sum == 45 && product == 362880 ) {
-                valid[row + col/3] = true;
+                isValid[row + col/3] = true;
             }
             // If reached this point, 3x3 subsection is valid.
              // Maps the subsection to an index in the first 8 indices of the valid array
@@ -44,7 +43,7 @@ public class test {
     public static void main(String[] args) {
         //Since we totally have 27 threads (9 * 3 = 27)
         int NUM_THREADS = 9;
-        valid = new boolean[NUM_THREADS];
+        isValid = new boolean[NUM_THREADS];
         Thread[] threads = new Thread[NUM_THREADS];
         int threadIndex = 0;
         // Create 9 threads for 9 3x3 subsections, 9 threads for 9 columns and 9 threads for 9 rows.
@@ -52,7 +51,7 @@ public class test {
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 if (i%3 == 0 && j%3 == 0) {
-                    threads[threadIndex++] = new Thread(new Is3x3Valid(i, j));
+                    threads[threadIndex++] = new Thread(new validator(i, j));
                 }
 
             }
@@ -73,8 +72,8 @@ public class test {
         }
 
         // If any of the entries in the valid array are 0, then the sudoku solution is invalid
-        for (int i = 0; i < valid.length; i++) {
-            if (!valid[i]) {
+        for (int i = 0; i < isValid.length; i++) {
+            if (!isValid[i]) {
                 System.out.println("Sudoku solution is invalid!");
                 return;
             }
